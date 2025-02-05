64d66134e9beb768a05c4bb7cfc1dc113ecd76ca
Paulo Henrique <paulosilvaponted@gmail.com>
2025-02-05 16:55:45 -0300

    Adicionar arquivos de projeto.

---
+++ basic10_calculadora/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace basic10_calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtB_calc = new System.Windows.Forms.TextBox();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_mais = new System.Windows.Forms.Button();
+            this.btn_ce = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_menos = new System.Windows.Forms.Button();
+            this.btn_c = new System.Windows.Forms.Button();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_multi = new System.Windows.Forms.Button();
+            this.btn_igual = new System.Windows.Forms.Button();
+            this.btn_0 = new System.Windows.Forms.Button();
+            this.btn_ponto = new System.Windows.Forms.Button();
+            this.btn_barra = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtB_calc
+            // 
+            this.txtB_calc.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtB_calc.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtB_calc.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtB_calc.Location = new System.Drawing.Point(46, 47);
+            this.txtB_calc.Name = "txtB_calc";
+            this.txtB_calc.Size = new System.Drawing.Size(306, 27);
+            this.txtB_calc.TabIndex = 0;
+            // 
+            // btn_7
+            // 
+            this.btn_7.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_7.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_7.Location = new System.Drawing.Point(46, 83);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(60, 45);
+            this.btn_7.TabIndex = 1;
+            this.btn_7.Text = "7";
+            this.btn_7.UseVisualStyleBackColor = false;
+            this.btn_7.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_8
+            // 
+            this.btn_8.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_8.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_8.Location = new System.Drawing.Point(111, 83);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(55, 45);
+            this.btn_8.TabIndex = 2;
+            this.btn_8.Text = "8";
+            this.btn_8.UseVisualStyleBackColor = false;
+            // 
+            // btn_9
+            // 
+            this.btn_9.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_9.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_9.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_9.Location = new System.Drawing.Point(172, 83);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(56, 45);
+            this.btn_9.TabIndex = 3;
+            this.btn_9.Text = "9";
+            this.btn_9.UseVisualStyleBackColor = false;
+            // 
+            // btn_mais
+            // 
+            this.btn_mais.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_mais.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_mais.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_mais.Location = new System.Drawing.Point(234, 83);
+            this.btn_mais.Name = "btn_mais";
+            this.btn_mais.Size = new System.Drawing.Size(58, 45);
+            this.btn_mais.TabIndex = 4;
+            this.btn_mais.Text = "+";
+            this.btn_mais.UseVisualStyleBackColor = false;
+            // 
+            // btn_ce
+            // 
+            this.btn_ce.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_ce.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ce.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_ce.Location = new System.Drawing.Point(298, 83);
+            this.btn_ce.Name = "btn_ce";
+            this.btn_ce.Size = new System.Drawing.Size(54, 45);
+            this.btn_ce.TabIndex = 5;
+            this.btn_ce.Text = "CE";
+            this.btn_ce.UseVisualStyleBackColor = false;
+            // 
+            // btn_4
+            // 
+            this.btn_4.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_4.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_4.Location = new System.Drawing.Point(46, 134);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(60, 45);
+            this.btn_4.TabIndex = 6;
+            this.btn_4.Text = "4";
+            this.btn_4.UseVisualStyleBackColor = false;
+            // 
+            // btn_5
+            // 
+            this.btn_5.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_5.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_5.Location = new System.Drawing.Point(112, 135);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(54, 44);
+            this.btn_5.TabIndex = 7;
+            this.btn_5.Text = "5";
+            this.btn_5.UseVisualStyleBackColor = false;
+            // 
+            // btn_6
+            // 
+            this.btn_6.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_6.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_6.Location = new System.Drawing.Point(173, 135);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(55, 44);
+            this.btn_6.TabIndex = 8;
+            this.btn_6.Text = "6";
+            this.btn_6.UseVisualStyleBackColor = false;
+            // 
+            // btn_menos
+            // 
+            this.btn_menos.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_menos.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_menos.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_menos.Location = new System.Drawing.Point(235, 135);
+            this.btn_menos.Name = "btn_menos";
+            this.btn_menos.Size = new System.Drawing.Size(57, 44);
+            this.btn_menos.TabIndex = 9;
+            this.btn_menos.Text = "-";
+            this.btn_menos.UseVisualStyleBackColor = false;
+            // 
+            // btn_c
+            // 
+            this.btn_c.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_c.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_c.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_c.Location = new System.Drawing.Point(299, 135);
+            this.btn_c.Name = "btn_c";
+            this.btn_c.Size = new System.Drawing.Size(53, 44);
+            this.btn_c.TabIndex = 10;
+            this.btn_c.Text = "C";
+            this.btn_c.UseVisualStyleBackColor = false;
+            // 
+            // btn_1
+            // 
+            this.btn_1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_1.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_1.Location = new System.Drawing.Point(46, 186);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(60, 46);
+            this.btn_1.TabIndex = 11;
+            this.btn_1.Text = "1";
+            this.btn_1.UseVisualStyleBackColor = false;
+            // 
+            // btn_2
+            // 
+            this.btn_2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_2.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_2.Location = new System.Drawing.Point(112, 186);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(54, 46);
+            this.btn_2.TabIndex = 12;
+            this.btn_2.Text = "2";
+            this.btn_2.UseVisualStyleBackColor = false;
+            this.btn_2.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_3.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_3.Location = new System.Drawing.Point(173, 186);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(55, 46);
+            this.btn_3.TabIndex = 13;
+            this.btn_3.Text = "3";
+            this.btn_3.UseVisualStyleBackColor = false;
+            // 
+            // btn_multi
+            // 
+            this.btn_multi.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_multi.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_multi.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_multi.Location = new System.Drawing.Point(235, 186);
+            this.btn_multi.Name = "btn_multi";
+            this.btn_multi.Size = new System.Drawing.Size(57, 46);
+            this.btn_multi.TabIndex = 14;
+            this.btn_multi.Text = "*";
+            this.btn_multi.UseVisualStyleBackColor = false;
+            // 
+            // btn_igual
+            // 
+            this.btn_igual.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_igual.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_igual.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_igual.Location = new System.Drawing.Point(298, 186);
+            this.btn_igual.Name = "btn_igual";
+            this.btn_igual.Size = new System.Drawing.Size(54, 93);
+            this.btn_igual.TabIndex = 15;
+            this.btn_igual.Text = "=";
+            this.btn_igual.UseVisualStyleBackColor = false;
+            // 
+            // btn_0
+            // 
+            this.btn_0.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_0.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_0.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_0.Location = new System.Drawing.Point(46, 239);
+            this.btn_0.Name = "btn_0";
+            this.btn_0.Size = new System.Drawing.Size(120, 40);
+            this.btn_0.TabIndex = 16;
+            this.btn_0.Text = "0";
+            this.btn_0.UseVisualStyleBackColor = false;
+            // 
+            // btn_ponto
+            // 
+            this.btn_ponto.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_ponto.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ponto.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_ponto.Location = new System.Drawing.Point(173, 239);
+            this.btn_ponto.Name = "btn_ponto";
+            this.btn_ponto.Size = new System.Drawing.Size(55, 40);
+            this.btn_ponto.TabIndex = 17;
+            this.btn_ponto.Text = ".";
+            this.btn_ponto.UseVisualStyleBackColor = false;
+            this.btn_ponto.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // btn_barra
+            // 
+            this.btn_barra.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_barra.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_barra.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_barra.Location = new System.Drawing.Point(235, 239);
+            this.btn_barra.Name = "btn_barra";
+            this.btn_barra.Size = new System.Drawing.Size(57, 40);
+            this.btn_barra.TabIndex = 18;
+            this.btn_barra.Text = "/";
+            this.btn_barra.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 308);
+            this.Controls.Add(this.btn_barra);
+            this.Controls.Add(this.btn_ponto);
+            this.Controls.Add(this.btn_0);
+            this.Controls.Add(this.btn_igual);
+            this.Controls.Add(this.btn_multi);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_1);
+            this.Controls.Add(this.btn_c);
+            this.Controls.Add(this.btn_menos);
+            this.Controls.Add(this.btn_6);
+            this.Controls.Add(this.btn_5);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.btn_ce);
+            this.Controls.Add(this.btn_mais);
+            this.Controls.Add(this.btn_9);
+            this.Controls.Add(this.btn_8);
+            this.Controls.Add(this.btn_7);
+            this.Controls.Add(this.txtB_calc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtB_calc;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button btn_mais;
+        private System.Windows.Forms.Button btn_ce;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_menos;
+        private System.Windows.Forms.Button btn_c;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_multi;
+        private System.Windows.Forms.Button btn_igual;
+        private System.Windows.Forms.Button btn_0;
+        private System.Windows.Forms.Button btn_ponto;
+        private System.Windows.Forms.Button btn_barra;
+    }
+}
+
